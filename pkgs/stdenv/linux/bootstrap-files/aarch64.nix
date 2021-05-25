812bdf9c6f9a831e06428fe043fb4856198a201b
Maximilian Bosch <maximilian@mbosch.me>
2021-05-25 23:02:58 +0200

    stdenv bootstrap: new aarch64 bootstrap files
    Also - of course - temporary. Also not entirely tested since the
    community-builder is also busy with other large builds :)

--- pkgs/stdenv/linux/bootstrap-files/aarch64.nix
+++ pkgs/stdenv/linux/bootstrap-files/aarch64.nix
@@ -1,11 +1,21 @@
 {
+  #busybox = import <nix/fetchurl.nix> {
+    #url = "http://tarballs.nixos.org/stdenv-linux/aarch64/bb3ef8a95c9659596b8a34d27881cd30ffea2f9f/busybox";
+    #sha256 = "12qcml1l67skpjhfjwy7gr10nc86gqcwjmz9ggp7knss8gq8pv7f";
+    #executable = true;
+  #};
+  #bootstrapTools = import <nix/fetchurl.nix> {
+    #url = "http://tarballs.nixos.org/stdenv-linux/aarch64/c5aabb0d603e2c1ea05f5a93b3be82437f5ebf31/bootstrap-tools.tar.xz";
+    #sha256 = "d3f1bf2a1495b97f45359d5623bdb1f8eb75db43d3bf2059fc127b210f059358";
+  #};
+
   busybox = import <nix/fetchurl.nix> {
-    url = "http://tarballs.nixos.org/stdenv-linux/aarch64/bb3ef8a95c9659596b8a34d27881cd30ffea2f9f/busybox";
-    sha256 = "12qcml1l67skpjhfjwy7gr10nc86gqcwjmz9ggp7knss8gq8pv7f";
+    url = "https://intranet.mayflower.de/s/ErP48RpqJXEgHzF/download/busybox";
+    sha256 = "sha256-dBSP8+pdGUYV/+WjU4TSiW6R63FtJaHntB1nHWkUXSA=";
     executable = true;
   };
   bootstrapTools = import <nix/fetchurl.nix> {
-    url = "http://tarballs.nixos.org/stdenv-linux/aarch64/c5aabb0d603e2c1ea05f5a93b3be82437f5ebf31/bootstrap-tools.tar.xz";
-    sha256 = "d3f1bf2a1495b97f45359d5623bdb1f8eb75db43d3bf2059fc127b210f059358";
+    url = "https://intranet.mayflower.de/s/MkefjzbLXAWaabJ/download/bootstrap-tools.tar.xz";
+    sha256 = "sha256-/9zizyulCjCUC7bRH2JDQc/UkYCOzT2GAxEvoaZ0Yrk=";
   };
 }
